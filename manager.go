c0d89b680ebf964dd304d5ec0db809e6a2fbfbfd
chenpro <chenxxpro@163.com>
2023-05-06 09:55:33 +0800

    refine

--- manager.go
+++ manager.go
@@ -55,7 +55,9 @@ func (m *Manager) Init() {
 }
 
 // Add 在指定地图坐标位置添加Entity。
+//
 // 此操作会触发OnEntityEnter函数回调
+//
 // @return 仅当成功添加entity时返回True，否则返回False
 func (m *Manager) Add(entity *Entity, position Coord) bool {
     verifyEntity(entity)
@@ -63,25 +65,28 @@ func (m *Manager) Add(entity *Entity, position Coord) bool {
         log.Println("ERROR: Tower manager add entity, coord INVALID, position=", position.String(), "entity=", entity)
         return false
     }
-    tc := m.convToTowerCoord(position)
-    return m.towers[tc.X][tc.Y].add(entity)
+    coord := m.convToTowerCoord(position)
+    return m.towers[coord.X][coord.Y].add(entity)
 }
 
 // Remove 从指定地图坐标位置移除Entity
+//
 // 此操作会触发OnEntityLeave函数回调
+//
 // @return 仅当成功删除entity时返回True，否则返回False
 func (m *Manager) Remove(entity *Entity) bool {
     verifyEntity(entity)
-    if entity.tower != nil {
-        p := *entity.tower
-        return m.towers[p.X][p.Y].remove(entity)
-    } else {
+    if entity.tower == nil {
         return false
     }
+    coord := *entity.tower
+    return m.towers[coord.X][coord.Y].remove(entity)
 }
 
-// Update 将Entity从指定地图坐标位置移动到新坐标位置
+// Update 将Entity从指定地图坐标位置移动到新坐标位置。
+//
 // 此操作会触发OnEntityEnter和OnEntityLeave函数回调
+//
 // @return 仅当成功更新entity时返回True，否则返回False
 func (m *Manager) Update(entity *Entity, from, to Coord) bool {
     verifyEntity(entity)
@@ -89,18 +94,18 @@ func (m *Manager) Update(entity *Entity, from, to Coord) bool {
         log.Println("ERROR: Tower manager update entity, coord INVALID, from=", from, "to=", to, "entity=", entity)
         return false
     }
-    tc1 := m.convToTowerCoord(from)
-    tc2 := m.convToTowerCoord(to)
-    if tc1.X > len(m.towers) || tc2.X > len(m.towers) {
-        log.Println("ERROR: Tower manager update entity, old.pos=", from, "old.at=", tc1, "new.pos=", to, "new.at", tc2)
+    prevCoord := m.convToTowerCoord(from)
+    nextCoord := m.convToTowerCoord(to)
+    if prevCoord.X > len(m.towers) || nextCoord.X > len(m.towers) {
+        log.Println("ERROR: Tower manager update entity, prev.position=", from, "prev.tower=", prevCoord, "next.positon=", to, "next.tower", nextCoord)
         return false
     }
     // Tower坐标没有发生切换
-    if tc1.X == tc2.X && tc1.Y == tc2.Y {
+    if prevCoord.X == nextCoord.X && prevCoord.Y == nextCoord.Y {
         return false
     }
-    m.towers[tc1.X][tc1.Y].remove(entity) // Old tower
-    m.towers[tc2.X][tc2.Y].add(entity)    // New tower
+    m.towers[prevCoord.X][prevCoord.Y].remove(entity) // Prev tower
+    m.towers[nextCoord.X][nextCoord.Y].add(entity)    // Next tower
     return true
 }
 
@@ -128,37 +133,37 @@ func (m *Manager) ClearWatcher(watcher *Watcher) {
     }
 }
 
-func (m *Manager) searchTowers(position Coord, dist int, onEach func(tower *Tower)) {
+func (m *Manager) searchTowers(position Coord, dist int, onTower func(tower *Tower)) {
     ip := m.convToTowerCoord(position)
     start, end := m.coordRangeOf(ip, dist, m.max)
-    for i := start.X; i <= end.X; i++ {
-        for j := start.Y; j <= end.Y; j++ {
-            onEach(m.towers[i][j])
+    for x := start.X; x <= end.X; x++ {
+        for y := start.Y; y <= end.Y; y++ {
+            onTower(m.towers[x][y])
         }
     }
 }
 
-func (m *Manager) coordRangeOf(pos ICoord, dist int, max ICoord) (start ICoord, end ICoord) {
-    if pos.X-dist < 0 {
+func (m *Manager) coordRangeOf(coord ICoord, dist int, max ICoord) (start ICoord, end ICoord) {
+    if coord.X-dist < 0 {
         start.X = 0
         end.X = 2 * dist
-    } else if pos.X+dist > max.X {
+    } else if coord.X+dist > max.X {
         end.X = max.X
         start.X = max.X - 2*dist
     } else {
-        start.X = pos.X - dist
-        end.X = pos.X + dist
+        start.X = coord.X - dist
+        end.X = coord.X + dist
     }
 
-    if pos.Y-dist < 0 {
+    if coord.Y-dist < 0 {
         start.Y = 0
         end.Y = 2 * dist
-    } else if pos.Y+dist > max.Y {
+    } else if coord.Y+dist > max.Y {
         end.Y = max.Y
         start.Y = max.Y - 2*dist
     } else {
-        start.Y = pos.Y - dist
-        end.Y = pos.Y + dist
+        start.Y = coord.Y - dist
+        end.Y = coord.Y + dist
     }
     start.X = iMax(start.X, 0)
     end.X = iMin(end.X, max.X)
